a4efb812d317af0f8462e65487721d95336d5e12
KUNAL BHADE <109821019+kunal15112001@users.noreply.github.com>
2022-10-09 17:38:02 +0530

    Create Palindrome.c

---
+++ C/Palindrome.c
@@ -0,0 +1,29 @@
+/*
+A palindrome number is a number that is same after reverse. 
+For example 121, 34543, 343, 131, 48984 are the palindrome numbers.
+*/
+#include <stdio.h>
+int main()
+{
+    int n, m, rem, rev=0;
+    printf("Enter a Number:");
+    scanf("%d",&n);
+    m=n;
+     
+     
+    while(m > 0)
+    {
+        rem = m % 10;
+        rev = rev * 10 +rem;
+        m = m / 10;
+    }
+    if(n == rev)
+    {
+        printf("%d is a Palindrome",n);
+    }
+    else
+    {
+        printf("%d is not a Palindrome",n);
+    }
+    return 0;
+}
